a86ca58ef6440fba25e57e66d356920424d62c2c
meghana-prog <meghanaboddu01@example.com>
2026-09-16 13:58:01 +0530

    Added migration files

---
+++ travel360_dbt/models/staging/stg_customers_codegen.sql
@@ -0,0 +1,25 @@
+﻿
+
+with source as (
+
+    select * from {{ source('travel360', 'customers') }}
+
+),
+
+renamed as (
+
+    select
+        customer_id,
+        first_name,
+        last_name,
+        email,
+        phone,
+        city,
+        country
+
+    from source
+
+)
+
+select * from renamed
+
